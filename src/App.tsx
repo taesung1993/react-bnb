e4bf91072c2e3ec38e56fbabc2a4c03beb75823e
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-01-31 21:31:30 +0900

    delete app.css and add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "@Components/Header";
+import Main from "@Components/Main";
+import Footer from "@Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="page-container">
+      <Header />
+      <Main />
+      <Footer />
+    </section>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <section></section>
+      <nav></nav>
+    </header>
+  );
+}
+
+export default Header;
